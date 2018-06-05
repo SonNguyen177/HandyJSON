11731588dafb74afe5c16ea5b4d2a4127f1a3549
Son Nguyen <son.nh@ifs.com.vn>
2018-06-05 17:09:46 +0700

    add DecimalTransform

--- Source/NSDecimalNumberTransform.swift
+++ Source/NSDecimalNumberTransform.swift
@@ -49,3 +49,25 @@ open class NSDecimalNumberTransform: TransformType {
         return value.description
     }
 }
+
+open class DecimalTransform: TransformType {
+    public typealias Object = Decimal
+    public typealias JSON = String
+    
+    public init() {}
+    
+    open func transformFromJSON(_ value: Any?) -> Decimal? {
+        if let string = value as? String {
+            return Decimal(string: string)
+        }
+        if let double = value as? Double {
+            return Decimal.init(double)
+        }
+        return nil
+    }
+    
+    open func transformToJSON(_ value: Decimal?) -> String? {
+        guard let value = value else { return nil }
+        return value.description
+    }
+}
